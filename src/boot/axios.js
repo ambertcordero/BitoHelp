d2eccb71359964c6594301fdabd4c4e0aad5f959
ambertcordero <corderoambert1996@gmail.com>
2026-04-01 09:23:09 +0800

    api update

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'http://localhost:8001/api/',
   headers: {
     'Content-Type': 'application/json',
   },
